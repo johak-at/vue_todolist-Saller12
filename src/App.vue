4f7da470a3c814dbfaad79dd17696a499e8e9a6e
Saller12 <saller.josef@johak.at>
2022-10-04 11:16:41 +0200

    imlemented basic add functionalitiy for todo list

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
 // Import your components here:
 import HelloJOHAK from "./components/HelloJOHAK.vue";
+import ToDoList from "./components/ToDoList.vue";
+function hello(){
+  alert("Uwe");
+}
 </script>
 
 <template>
   <!-- Use your components here: -->
-  <HelloJOHAK></HelloJOHAK>
+  <ToDoList></ToDoList>
+  <br>
+  <button @click="hello">Click Me!!</button>
   <div>
     <p>
       built with <img src="/vite.svg" alt="vite-logo" /> &
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from "vue";
+const newItem = ref("");
+const list = ref([
+]);
+function addButton()
+{
+    list.value.push(newItem.value);
+    newItem.value = "";
+    
+}
+function deleteButton(index)
+{
+list.value.splice(index,1);
+}
+
+
+</script>
+
+<template>
+    <h1>ToDoList</h1>
+    <button @click="addButton">Add Item!</button>
+    <input type="text" placeholder="Input Item" v-model="newItem">
+        <ul>
+            <li v-for="item in list" :key="item">
+                    {{item}}
+                    <button @click="deleteButton">X</button>
+            </li>
+           
+        </ul>
+</template >
+
+<style>
+
+</style>
+
+
